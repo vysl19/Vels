103606cf55e7531e937ccbe31fb4e39e63559d69
veysel <veysel.kurhan@paycore.com>
2022-05-29 18:22:55 +0300

    Added Person and ContactControllers

---
+++ ContactService/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using ContactService.Data;
+using ContactService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContactService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactRepository _contactRepository;
+        public ContactController(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }        
+
+        // POST api/<ContactController>
+        [HttpPost]
+        public void Post([FromBody] Contact contact)
+        {
+            _contactRepository.Add(contact);
+            _contactRepository.SaveChanges();            
+        }       
+
+        // DELETE api/<ContactController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _contactRepository.Delete(id);
+            _contactRepository.SaveChanges();
+        }
+    }
+}
---
+++ ContactService/Controllers/PersonController.cs
@@ -0,0 +1,55 @@
+﻿using ContactService.Data;
+using ContactService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContactService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _personRepository;
+        public PersonController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+        // GET: api/<PersonController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetPersons()
+        {
+            var persons=  _personRepository.List();
+            return Ok(persons);
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Person> Get(string id)
+        {
+            var person = _personRepository.Get(id);
+            if(person== null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] Person person)
+        {
+            _personRepository.Add(person);
+            _personRepository.SaveChanges();
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _personRepository.Delete(id);
+            _personRepository.SaveChanges();
+        }
+    }
+}
